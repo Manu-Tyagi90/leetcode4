0577457d79aff5728b04c048b7cad2b9ec4a55b4
Manu Tyagi <manutygii90@gmail.com>
2025-01-20 21:24:12 +0530

    p2.java

--- p2.java
+++ p2.java
@@ -1,61 +1,50 @@
 package leetcode1;
-// https://leetcode.com/problems/add-two-numbers/
-public class p2 {
-    public static void main(String[] args) {
-        ListNode l1 = new ListNode(5);
-        l1.next = new ListNode(3);
-        l1.next.next = new ListNode(4);
-
-        ListNode l2 = new ListNode(9);
-        l2.next = new ListNode(1);
-        l2.next.next = new ListNode(2);
-        
-        Solution1 solution = new Solution1();
-        ListNode Result = solution.addTwoNumbers(l1,l2);
-        
-        while (Result!=null) {
-            System.out.print(Result.val);
-            Result = Result.next;
-        }
-    
-    }
-    
-    
-}
-
-class Solution1{
-    public ListNode addTwoNumbers(ListNode L1,ListNode L2){
-        ListNode Dummy = new ListNode(0);
-        ListNode Current = Dummy;
-
-        while (L1!=null && L2!=null ) {
-            if(L1.val<L2.val){
-                Current.next = L1;
-                L1=L1.next;
-            }
-
-            else if(L2.val<=L1.val){
-                Current.next = L2;
-                L2=L2.next;
-            }
-            Current = Current.next;
-        }
-
-        if(L1!=null){
-            Current.next = L1;
-        }
-        else{
-            Current.next = L2;
-        }
-
-        return Dummy.next;
-    }
-}
-
-class ListNode{
+// https://leetcode.com/problems/merge-two-sorted-lists/
+class ListNode {
     int val;
     ListNode next;
-    ListNode(int val){
-        this.val = val;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class p2 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else if (list2 != null) {
+            current.next = list2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        // Example usage
+        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
+
+        p2 solution = new p2();
+        ListNode mergedList = solution.mergeTwoLists(list1, list2);
+
+        // Print the merged list
+        while (mergedList != null) {
+            System.out.print(mergedList.val + " ");
+            mergedList = mergedList.next;
+        }
     }
 }
